ca60ae47442f0dc5c81aa9810f9abc62dc97a789
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-03 17:00:46 -0500

    Add files via upload

---
+++ Chapter_3_Introducing_Lists/Exercise_3-2_Greetings.py
@@ -0,0 +1,7 @@
+print("Start with the list you used in Exercise 3-1, but instead of just printing each person\'s name, print a message to them. The text of each method should be the same, but each message should be personalized with the person\'s name.")
+
+names = ['John', 'Shaun', 'Corey']
+message = "Hello I would like you to meet "
+print(message + names[0] + ".")
+print(message + names[1] + ".")
+print(message + names[2] + ".")
