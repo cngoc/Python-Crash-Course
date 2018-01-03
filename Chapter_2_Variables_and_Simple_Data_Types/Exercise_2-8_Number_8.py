8104fdb068d2e2fff63e737cf8a234a55141f89f
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-03 09:28:04 -0500

    Add files via upload

---
+++ Chapter_2_Variables_and_Simple_Data_Types/Exercise_2-8_Number_8.py
@@ -0,0 +1,9 @@
+print("Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.")
+
+print("\n")
+print(5 + 3)
+print(10 - 2)
+print(1 * 8)
+print(8 / 1)
+
+# In python 3.6.4 8/1 returns a float. Maybe later in the book it'll teach us how to address this.
