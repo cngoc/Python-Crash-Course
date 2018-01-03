cf0b1fafee7ea3b79e4c82dbbcdbc94864662680
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-03 08:21:22 -0500

    Add files via upload

---
+++ Chapter_2_Variables_and_Simple_Data_Types/Exercise_2-6_Famous_Quote2.py
@@ -0,0 +1,6 @@
+print("Repeat Exercise 2-5, but this time store the famous person\'s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.")
+
+famous_person = "Tchaikovsky"
+message = "\nIf we wait for the mood, without endeavoring to meet it halfway, we easily become indolent and apathetic.\""
+
+print(message + "\n-" + famous_person)
