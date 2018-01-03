d368513d1d6dcdc8c15b57f03969402a58b77359
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-03 09:34:06 -0500

    Add files via upload

---
+++ Chapter_2_Variables_and_Simple_Data_Types/Exercise_2-9_Favorite_Number.py
@@ -0,0 +1,4 @@
+print("Store your favorite number in a variale. Then, using that variable, create a message that reveals your favorite number. Print that message.")
+
+fav_num = 23
+print(str(fav_num) + " is my favorite number, I think.")
