fd5be15e648e9e9196ef69748bdf88c7d9d3d835
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-03 08:05:38 -0500

    Add files via upload

---
+++ Chapter_2_Variables_and_Simple_Data_Types/Exercise_2-4_Name_Cases.py
@@ -0,0 +1,6 @@
+print("Store a person\'s name in a variable, and then print that person\'s name in lowercase, uppercase, and titlecase.")
+
+name = "FluFFy"
+print(name.lower())
+print(name.upper())
+print(name.title())
