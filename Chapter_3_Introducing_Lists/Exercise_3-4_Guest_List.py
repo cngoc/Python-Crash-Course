8ee95dce15b5007a28bdb68b91197b4792b4bcbb
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-04 14:02:03 -0500

    Add files via upload

---
+++ Chapter_3_Introducing_Lists/Exercise_3-4_Guest_List.py
@@ -0,0 +1,9 @@
+print("If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.")
+
+invitations = ['Clifford the Big Red Dog', 'Franklin the Turtle', 'Arthur the Bear']
+invite = ", you have been invited to my dinner!"
+
+print('\n')
+print(invitations[0] + invite)
+print(invitations[1] + invite)
+print(invitations[2] + invite)
