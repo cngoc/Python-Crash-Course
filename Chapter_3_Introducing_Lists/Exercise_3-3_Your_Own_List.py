050677a87cfce12a213a8e7e629662e02dd70f59
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-04 09:44:05 -0500

    Add files via upload

---
+++ Chapter_3_Introducing_Lists/Exercise_3-3_Your_Own_List.py
@@ -0,0 +1,10 @@
+print("think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as \"I would like to own a Honda motorcycle.\"")
+
+bikes = ['focus', 'bianchi', 'giant', 'specialized', 'trek']
+
+print("\n")
+print("I currently own a " + bikes[0].title() + ".")
+print("I don't own a " + bikes[3].title() + ", but I do own a fancy bike seat they make.")
+print("My first bike was a " + bikes[2].title() + ".")
+print("I love the " + bikes[1].title() + " blue. It\'s the only reason why I want a " + bikes[1].title() + ".")
+print("I\'ve never considered owning a " + bikes[4].title() + ".")
