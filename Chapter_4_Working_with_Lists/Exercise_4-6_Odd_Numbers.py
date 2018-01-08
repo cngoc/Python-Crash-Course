ff1078a014c7663cb1503222405680a088ff7c2a
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-08 14:12:12 -0500

    Add files via upload

---
+++ Chapter_4_Working_with_Lists/Exercise_4-6_Odd_Numbers.py
@@ -0,0 +1,5 @@
+print("Use the third argument of the range() function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number.\n")
+
+odd_numbers = list(range(1, 21, 2))
+
+print(odd_numbers)
