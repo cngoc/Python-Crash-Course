c56f08b2a35860a8ebf4068dfe8e0a314af470bc
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-08 15:44:34 -0500

    Add files via upload

---
+++ Chapter_4_Working_with_Lists/Exercise_4-7_Threes.py
@@ -0,0 +1,6 @@
+print("Make a list of the multiples of 3 from 3 to 30. Use a for loop to print the numbers in your list.")
+
+three_multiples = list(range(3,31,3))
+
+for three_multiple in three_multiples:
+    print("A multiple of 3 from 3 to 30 are " + str(three_multiple))
