d3d8dfee77efc6c2542d44859009a436000e0113
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-08 13:00:49 -0500

    Adding comments
    Adding comments to completed program.

--- Chapter_4_Working_with_Lists/Exercise_4-3_Counting_to_Twenty.py
+++ Chapter_4_Working_with_Lists/Exercise_4-3_Counting_to_Twenty.py
@@ -1,6 +1,9 @@
+#instructions
 print("Use a for loop to print the numbers from 1 to 20, inclusive \n")
 
+#creating a list of number 1-20 using the list() and range() function.
 numbers = list(range(1,21))
 
+#printing the numbers in the list.
 for number in numbers:
     print(number)
