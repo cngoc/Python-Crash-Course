34387e1ca59d087d7b77fff9a0029a93a361c452
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-01-29 09:44:35 -0500

    Add files via upload

---
+++ Chapter_7_User_Input_and_While_Loops/7-7_Infinity.py
@@ -0,0 +1,5 @@
+# Write a loop that never ends, and run it. (To end the loop, press CTRL-C
+# or close the window displaying the output.)
+
+while 1 < 2:
+    print ("How will 1 ever become => to 2?")
