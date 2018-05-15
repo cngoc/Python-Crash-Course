f531d15365020a57cd3c2a1df10e382813e10403
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-05-15 15:26:50 -0400

    Add files via upload

---
+++ Chapter_10_Files_and_Exceptions/10-2_Learning_C.py
@@ -0,0 +1,14 @@
+#You can use the replace() method to replace any word in a string with a
+#different word. Here's a quick example showing how to replace 'dog' with 'cat'
+#in a sentence:
+#message = "I really like dogs."
+#message.replace('dog','cat')
+#'I really like cats.'
+#Read in each line from the file you just created, learning_python.txt, and
+#replace the word Python with the name of another language, such as C.
+#Print each modified line to the screen.
+
+
+with open('learning_python.txt') as file_object:
+    for line in file_object:
+        print(line.replace('Python', 'C').rstrip())
