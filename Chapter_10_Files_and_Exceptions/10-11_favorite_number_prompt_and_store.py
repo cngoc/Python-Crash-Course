01b3594562040a45f21cfaa285cef34ce657015f
Chandara Ngoc <ngoc.chandara@gmail.com>
2018-08-07 15:46:19 -0400

    Update 10-11_favorite_number_prompt_and_store.py
    Forgot to import json

--- Chapter_10_Files_and_Exceptions/10-11_favorite_number_prompt_and_store.py
+++ Chapter_10_Files_and_Exceptions/10-11_favorite_number_prompt_and_store.py
@@ -1,5 +1,6 @@
 #Write a program that prompts for the user's favorite number. Use json.dump() to store this number in a file.
 #Write a separate program that reads in this value and prints the message, "I know your favorite number! It's ...!"
+import json
 
 fav_num = input("What is your favorite number? ")
 filename = 'favNum.json'
